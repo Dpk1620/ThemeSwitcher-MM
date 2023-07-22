941f7c8923c5548e2a6901c1dfaf537d0434a6ac
deepak <d1620k@yahoo.com>
2023-07-23 04:57:40 +0530

    changes

--- src/Pages/Analytics.jsx
+++ src/Pages/Analytics.jsx
@@ -5,14 +5,14 @@ import { PieChart, Pie } from 'recharts';
  const Analytics = () => {
   
   const data = [
-    { name: 'Geeksforgeeks', students: 400, fill:"red" ,y: 32, label: "Health",titlName:"Geeksforgeeks" },
-    { name: 'Technical scripter', students: 700 ,fill:"green",y: 22, titlName: "Finance",titlName:"Technical scripter"},
-    { name: 'Geek-i-knack', students: 200 ,fill:"yellow",titlName:"Geek-i-knack"},
-    { name: 'Geek-o-mania', students: 1000 ,fill:"orange",titlName:"Geek-o-mania"},
-    { name: 'Geek-o-mania', students: 400 ,fill:"voilet",titlName:"Technical scripter"},
-    { name: 'Technical scripter', students: 700 ,fill:"rose",titlName:"Geek-o"},
-    { name: 'Geek-i-knack', students: 200 ,fill:"brown",titlName:"Geeksgeeks"},
-    { name: 'Geek-o-mania', students: 1000,fill:"aqua",titlName:"Geek-i-knack" },
+    { name: 'Geeksforgeeks', students: 400, fill:"red" , },
+    { name: 'Technical scripter', students: 700 ,fill:"green",y: 22, },
+    { name: 'Geek-i-knack', students: 200 ,fill:"yellow",},
+    { name: 'Geek-o-mania', students: 1000 ,fill:"orange",},
+    { name: 'Geek-o-mania', students: 400 ,fill:"voilet",},
+    { name: 'Technical scripter', students: 700 ,fill:"rose",},
+    { name: 'Geek-i-knack', students: 200 ,fill:"brown",},
+    { name: 'Geek-o-mania', students: 1000,fill:"aqua", },
     
 
 ];
