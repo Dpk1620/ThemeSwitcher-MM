734d3814bbd26f4852388683f4a820996e719610
deepak <d1620k@yahoo.com>
2023-07-23 04:52:12 +0530

    changes

--- src/Pages/Analytics.jsx
+++ src/Pages/Analytics.jsx
@@ -5,21 +5,21 @@ import { PieChart, Pie } from 'recharts';
  const Analytics = () => {
   
   const data = [
-    { name: 'Geeksforgeeks', students: 400, fill:"red" ,y: 32, label: "Health",label:"Geeksforgeeks" },
-    { name: 'Technical scripter', students: 700 ,fill:"green",y: 22, label: "Finance",label:"Technical scripter"},
-    { name: 'Geek-i-knack', students: 200 ,fill:"yellow",label:"Geek-i-knack"},
-    { name: 'Geek-o-mania', students: 1000 ,fill:"orange",label:"Geek-o-mania"},
-    { name: 'Geek-o-mania', students: 400 ,fill:"voilet",label:"Technical scripter"},
-    { name: 'Technical scripter', students: 700 ,fill:"rose",label:"Geek-o"},
-    { name: 'Geek-i-knack', students: 200 ,fill:"brown",label:"Geeksgeeks"},
-    { name: 'Geek-o-mania', students: 1000,fill:"aqua",label:"Geek-i-knack" },
+    { name: 'Geeksforgeeks', students: 400, fill:"red" ,y: 32, label: "Health",titlName:"Geeksforgeeks" },
+    { name: 'Technical scripter', students: 700 ,fill:"green",y: 22, titlName: "Finance",titlName:"Technical scripter"},
+    { name: 'Geek-i-knack', students: 200 ,fill:"yellow",titlName:"Geek-i-knack"},
+    { name: 'Geek-o-mania', students: 1000 ,fill:"orange",titlName:"Geek-o-mania"},
+    { name: 'Geek-o-mania', students: 400 ,fill:"voilet",titlName:"Technical scripter"},
+    { name: 'Technical scripter', students: 700 ,fill:"rose",titlName:"Geek-o"},
+    { name: 'Geek-i-knack', students: 200 ,fill:"brown",titlName:"Geeksgeeks"},
+    { name: 'Geek-o-mania', students: 1000,fill:"aqua",titlName:"Geek-i-knack" },
     
 
 ];
   return (
     <div className='pieChartData'>
         <PieChart width={700} height={440}>
-            <Pie data={data} dataKey="students" y-axis={(data)=>{return data.y}} label={(data) => { return data.label}} outerRadius={200} fill={data.fill}  />
+            <Pie data={data} dataKey="students" y-axis={(data)=>{return data.y}} label={(data) => { return data.titlName}} outerRadius={200} fill={data.fill}  />
         </PieChart>
      </div>
   )
