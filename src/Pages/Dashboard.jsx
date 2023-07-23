f22ae408d23f3ac0d0e31b9f53fa038fd6022726
deepak <d1620k@yahoo.com>
2023-07-23 06:33:14 +0530

    changes done

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -5,7 +5,7 @@ const Dashboard = () => {
   const [users, setUsers] = useState([])
 
   const fetchData = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users/")
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
     const data = await response.json()
     setUsers(data)
   }
@@ -24,7 +24,7 @@ const Dashboard = () => {
             {users.map((user, index) => (
               <div className="mainCentral" style={{ backgroundColor: color }} key={user.id}>
                 <div className="card">
-                  <div className="card-title" style={{ padding: "5px", width: "auto", height: "70px" }}>
+                  <div className="card-title" style={{ padding: "10px", width: "auto", height: "80px" }}>
                     <div className="fontSizecard">
                       <span>
                         <b>Name:</b> <span style={{ color: color }}>{user.name}</span>
